28f09dd3f67b0c681a0320c34aa1f55850bbc519
arisarae <playarisa24@gmail.com>
2025-07-31 15:43:36 +0700

    chore: restructure folders for LandingPage components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,11 @@
 // src/app/page.tsx
 
-import HeroSection from '@/components/HeroSection'; // Ini sudah benar, karena index.tsx adalah entry point default
+import HeroSection from '@/containers/LandingPage/components/HeroSection'; // Ini sudah benar, karena index.tsx adalah entry point default
 import PublicNavbar from '@/components/Navbar/Public'; // Sesuaikan path ini
 import Footer from '@/components/Footer'; // Sesuaikan path ini
-import MainFeatures from '@/components/MainFeatures';
-import CaraKerja from '@/components/CaraKerja';
-import LastSection from '@/components/LastSection';
+import MainFeatures from '@/containers/LandingPage/components/MainFeatures';
+import CaraKerja from '@/containers/LandingPage/components/CaraKerja';
+import LastSection from '@/containers/LandingPage/components/LastSection';
 
 export default function Home() {
   return (
